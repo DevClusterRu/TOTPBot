123754da28087459d549f0843135d18ea79ba746
Den <webkrilya@gmail.com>
2021-08-18 14:08:30 +0700

    first

--- app/telegram.go
+++ app/telegram.go
@@ -4,6 +4,8 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/xlzd/gotp"
 	"log"
+	"strconv"
+	"strings"
 )
 
 func Generator_otp(Key string) string {
@@ -29,23 +31,23 @@ func StartBot() {
 
 	for update := range updates {
 
-		//id := update.Message.From.ID
+		id := update.Message.From.ID
 
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
 		}
 		if update.Message.Text == "give" {
 
-			//Key := Connect_db(strconv.Itoa(id))
-
-			Key := GetUserToken("alexandrov.v")
-
+			login := Connect_db(strconv.Itoa(id))
+			if strings.TrimSpace(login)==""{
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Такой пользователь не найден")
+				bot.Send(msg)
+			}
+			Key := GetUserToken(login)
 			if Key == "" {
 				continue
 			}
-
 			code := Generator_otp(Key)
-
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, code)
 			msg.ReplyToMessageID = update.Message.MessageID
 
--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"otpbot/app"
 )
 
 func main() {
 
-login := app.Connect_db("123")
-fmt.Println(login)
-	//app.StartBot()
+	app.StartBot()
 }
 
 
